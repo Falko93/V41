3a2ec0541db4707fbaff1cd584a99cbe8cd46ff4
Egor Evsenin <egor@gutschank.eu>
2018-11-19 11:24:26 +0100

    Funktion Strukturamplitude

--- auswertung.py
+++ auswertung.py
@@ -28,8 +28,8 @@ radius_m = np.array([4.5, 6.25, 8.2, 9.8, 11.5]) # in cm
 
 #Bogenlängenformel b=alpha*r*pi /180
 
-theta_4 = radius_m*180/(np.pi*Radius_k)
-theta = theta_4*np.pi/(4*180) # in radianten
+theta_4 = radius_m * 180 / (np.pi * Radius_k)
+theta = theta_4 * np.pi / (4 * 180) # in radianten
 
 print(' Theta=', theta)
 theta_sin = np.sin(theta)
@@ -40,7 +40,7 @@ print('sin(\theta) = ', theta_sin)
 
 
 def bragg(n, lambdaa, theta):
-    return n*lambdaa/(2*np.sin(theta))
+    return n * lambdaa / (2 * np.sin(theta))
 
 netzebenenabstand_1 = bragg(1, lambda_1, theta[0])
 
@@ -49,3 +49,58 @@ netzebenenabstand_1 = bragg(1, lambda_1, theta[0])
 #print('Netzebenenabstand',netzebenenabstand_1)
 
 ######### Streuamplitude ######################
+
+
+############ Metall ################
+
+### Nicht verschwindende Reflexe werden mit der Strukturamplitude berechnet. Es werden alle hkl Kombinationen von 100 bis 999 betrachtet wobei daruf geachtet wird, dass m = h^2 + k^2+ l^2 sich nicht doppelt.
+
+def Strukturamplitude(f, gitter = 'SC'):
+	print('Strukturamplitude berechnet für ' + gitter * '.')
+	if gitter == 'SC':
+		r = np.array([[0,0,0]])
+	if gitter == 'BCC':
+		r = np.array([[0,0,0],[.5,.5,.5]])
+	if gitter == 'FCC':
+		r = np.array([[0,0,0],[.5,.5,0],[.5,0,.5],[0,.5,.5]])
+	if gitter == 'Diamant':
+		r = np.array([[0,0,0],[.5,.5,0],[.5,0,.5],[0,.5,.5],[0.25,.25,.25],[.75,.75,.25],[.75,.25,.75],[.25,.75,.75]])
+
+	m = []
+	temp = 0
+	S = 0
+	reflexe = []
+	for h in range(0,9):
+		for k in range(0,9):
+			for l in range(0,9):
+				temp = h**2 + k**2 + l**2
+				if temp not in m:
+					for i in range(0,len(r) + 1):
+						S = f * np.exp(-2j * np.pi * (r[i][0] * h + r[i][1] * k + r[i][2] * l))
+				if S != 0:
+					reflexe.append([h, k, l])
+				else:
+					pass
+
+	return reflexe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
