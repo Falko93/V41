71f12836b5462e7df6d048140cd20a010071315e
Egor Evsenin <egor@gutschank.eu>
2018-11-20 17:55:11 +0100

    d's und m's berechnet und jetzt noch den Unterschied analytisch vernünftig darstellen und dann passt das

--- auswertung.py
+++ auswertung.py
@@ -19,31 +19,12 @@ from scipy import integrate
 #Strukturfaktoren berechnen
 ###############################################################################
 
-# lambda_1 = 1.5417e-10
-# Radius_k = 5.73 #in cm
-# radius_rohr = 0.4e-3
+lambda_1 = 1.5417e-10
+Radius_k = 5.73 #in cm
+radius_rohr = 0.4e-3
 
 
-# radius_m = np.array([4.5, 6.25, 8.2, 9.8, 11.5]) # in cm
-
-# #Bogenlängenformel b=alpha*r*pi /180
-
-# theta_4 = radius_m * 180 / (np.pi * Radius_k)
-# theta = theta_4 * np.pi / (4 * 180) # in radianten
-
-# print(' Theta=', theta)
-# theta_sin = np.sin(theta)
-# print('sin(\theta) = ', theta_sin)
-
-
-# ######### Bragg Bedingung ##################
-
-
-# def bragg(n, lambdaa, theta):
-#     return n * lambdaa / (2 * np.sin(theta))
-
-# netzebenenabstand_1 = bragg(1, lambda_1, theta[0])
-
+radius_m = np.array([4.5, 6.25, 8.2, 9.8, 11.5]) # in cm
 
 
 #print('Netzebenenabstand',netzebenenabstand_1)
@@ -86,8 +67,48 @@ def Strukturamplitude(f = 1, gitter = 'SC'): # gitter = 'SC', 'BCC', 'FCC', 'Dia
 
 	return reflexe
 
+######### Bragg Bedingung ##################
+
+
+def bragg(lambdaa, theta):
+    return lambdaa / (2 * np.sin(theta))
+
+
+
 def main():
-	print(Strukturamplitude(gitter = 'SC'))
+	gitter = 'FCC'
+	print(Strukturamplitude(gitter = gitter))
+
+	reflexe = Strukturamplitude(gitter = gitter)
+
+	#Bogenlängenformel b=alpha*r*pi /180
+	
+	theta_4 = radius_m * 180 / (np.pi * Radius_k)
+	theta = theta_4 * np.pi / (4 * 180) # in radianten
+	
+	print(' Theta=', theta)
+	theta_sin = np.sin(theta)
+	print('sin(\theta) = ', theta_sin)
+	
+	netzebenenabstand_1 = bragg(lambda_1, theta)
+	verhaeltnis_d = []
+	verhaeltnis_m = []
+	temp=0
+
+	for i in range(0,len(netzebenenabstand_1)):
+		for j in range(i+1,len(netzebenenabstand_1)):
+			verhaeltnis_d.append(netzebenenabstand_1[i] / netzebenenabstand_1[j])
+			m1 = (reflexe[i][0]**2 + reflexe[i][1]**2 + reflexe[i][2]**2)
+			m2 = (reflexe[j][0]**2 + reflexe[j][1]**2 + reflexe[j][2]**2)
+			verhaeltnis_m.append( np.sqrt(m2) / np.sqrt(m1) )
+
+	print('Verhältnisse der Netzebenenabstände für ' + gitter + ': ')
+	print(verhaeltnis_d)
+	print('Verhältnisse der ms für ' + gitter + ': ')
+	print(verhaeltnis_m)
+
+	############### ab hier muss die Xi^2 Methode von Simon hin..sieht sehr sinnvoll aus!
+		
 
 main()
 
