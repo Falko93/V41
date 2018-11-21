d55dae348a4143cf3d5cea73441bda8f39cb479c
Egor Evsenin <egor@gutschank.eu>
2018-11-21 11:50:19 +0100

    Extrapolation der Gitterkonstante für Metall fertig

--- auswertung.py
+++ auswertung.py
@@ -11,13 +11,6 @@ from scipy.optimize import curve_fit
 from scipy.stats import sem
 from scipy import integrate
 
-###############################################################################
-#Metall
-###############################################################################
-
-###############################################################################
-#Strukturfaktoren berechnen
-###############################################################################
 
 lambda_1 = 1.5417e-10
 Radius_k = 5.73 #in cm
@@ -25,14 +18,6 @@ radius_rohr = 0.4e-3
 
 
 radius_m = np.array([4.5, 6.25, 8.2, 9.8, 11.5]) # Radius der Kreibögen in cm für Metall
-
-
-#print('Netzebenenabstand',netzebenenabstand_1)
-
-######### Streuamplitude ######################
-
-
-############ Metall ################
 
 ### Nicht verschwindende Reflexe werden mit der Strukturamplitude berechnet. Es werden alle hkl Kombinationen von 100 bis 999 betrachtet wobei daruf geachtet wird, dass m = h^2 + k^2+ l^2 sich nicht doppelt.
 
@@ -93,6 +78,17 @@ def abweichung(verhaeltnis_m, verhaeltnis_d):
 
 	return Xi2
 
+def gitterkonstanteBragg(m, d):
+	a = []
+
+	for i in range(0,len(d)):
+		a.append(np.sqrt(m[i]) * d[i])
+
+	return a
+
+def lin(x, a, b):
+	return a * x + b
+
 
 def main():
 	# print(Strukturamplitude(gitter = 'SC'))
@@ -100,20 +96,18 @@ def main():
 	# print(Strukturamplitude(gitter = 'BCC'))
 	gitter_moegl = ['SC','FCC','BCC','Diamant']
 	Xi2_best = ['SC', 10]
-	# print(Strukturamplitude(gitter = gitter))
+
+	######################## Metall ##########################
+	#Bogenlängenformel b=alpha*r*pi /180
+	theta_4 = radius_m * 180 / (np.pi * Radius_k)
+	theta = theta_4 * np.pi / (4 * 180) # in radianten
+	theta_sin = np.sin(theta)
+	netzebenenabstand_metall = bragg(lambda_1, theta)
+
 	for gitter in gitter_moegl:
 		infos = Strukturamplitude(gitter = gitter)
 		reflexe = infos[0]
 		m = infos[1]
-	
-		######################## Metall ##########################
-		#Bogenlängenformel b=alpha*r*pi /180
-		theta_4 = radius_m * 180 / (np.pi * Radius_k)
-		theta = theta_4 * np.pi / (4 * 180) # in radianten
-		
-		theta_sin = np.sin(theta)
-		
-		netzebenenabstand_metall = bragg(lambda_1, theta)
 	
 		verhaeltnisse = findStructure(m, netzebenenabstand_metall)
 		verhaeltnis_m = verhaeltnisse[0]
@@ -128,25 +122,31 @@ def main():
 
 	print('Struktur mit der kleinsten Abweichung: ', Xi2_best[0])
 	print('Abweichung Xi^2: ', Xi2_best[1])
-
-	# verhaeltnis_d = []
-	# verhaeltnis_m = []
-	# temp=0
-
-	# for i in range(0,len(netzebenenabstand_1)):
-	# 	for j in range(i+1,len(netzebenenabstand_1)):
-	# 		verhaeltnis_d.append(netzebenenabstand_1[i] / netzebenenabstand_1[j])
-	# 		m1 = (reflexe[i][0]**2 + reflexe[i][1]**2 + reflexe[i][2]**2)
-	# 		m2 = (reflexe[j][0]**2 + reflexe[j][1]**2 + reflexe[j][2]**2)
-	# 		verhaeltnis_m.append( np.sqrt(m2) / np.sqrt(m1) )
-
-	# print('Verhältnisse der Netzebenenabstände für ' + gitter + ': ')
-	# print(verhaeltnis_d)
-	# print('Verhältnisse der ms für ' + gitter + ': ')
-	# print(verhaeltnis_m)
-
-	############### ab hier muss die Xi^2 Methode von Simon hin..sieht sehr sinnvoll aus!
 		
+	m = Strukturamplitude(gitter = Xi2_best[0])[1]
+	a = np.array(gitterkonstanteBragg(m, netzebenenabstand_metall))
+
+	print('Gitterkonstanten für ' + Xi2_best[0] + ' Struktur: ', a)
+
+	params, cov = curve_fit(lin,np.cos(theta)**2,a)
+	err = np.sqrt(np.diag(cov))
+
+	a_extrp = ufloat(params[1], err[1])
+
+	print('Extrapolierte Gitterkonstante: ', a_extrp)
+
+	cos2Theta_fit = np.linspace(0, 1)
+
+	plt.plot(np.cos(theta)**2, a, 'x', label = 'Daten')
+	plt.plot(cos2Theta_fit, lin(cos2Theta_fit, *params), label = 'Fit')
+	plt.legend(loc = 'best')
+	plt.xlabel('cos$^2(\Theta)$')
+	plt.ylabel('$a$ in Angtröm')
+	plt.xlim(0.6,1)
+	plt.tight_layout()
+	plt.grid()
+	plt.savefig('Plots/Metall_Fit.pdf')
+
 
 main()
 
